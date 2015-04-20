d02e020c6e7c34fbbaec2fde40153db5600350a7
Josh Hofing <joshhofing@gmail.com>
2015-04-20 15:59:59 -0400

    Make stuff laaazy

--- pyutil.py
+++ pyutil.py
@@ -1,15 +1,19 @@
-
+from itertools import izip
 
 def groups_of(itr, n=8):
     """ Return groups of n consecutive elements from itr
         n defaults to 8
+
+        Returns a generator
     """
-    return zip(*[iter(itr)]*n)
+    return izip(*[iter(itr)]*n)
 
 def every_nth(itr, n=8, offset=0):
     """ Return every nth element of itr, starting with offset
         n defaults to 8, offset 0
 
         offset must be less than n
+
+        Returns a generator
     """
-    return [group[offset] for group in groups_of(itr, n)]
+    return (group[offset] for group in groups_of(itr, n))
