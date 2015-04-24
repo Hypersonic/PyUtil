d3d2e8df6385cd3b0f67a6b5812d6ff7af9aadcf
Josh Hofing <joshhofing@gmail.com>
2015-04-23 20:33:20 -0400

    Add save_file

--- pyutil.py
+++ pyutil.py
@@ -38,3 +38,10 @@ def load_file(filename):
     """
     with open(filename) as f:
         return f.read()
+
+def save_file(filename, contents):
+    """ Open filename and dump contents to it.
+        If the file exists, it is overwritten
+    """
+    with open(filename, 'w') as f:
+        f.write(contents)
