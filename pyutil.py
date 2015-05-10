272d86410bf348f837bd8fbfbfff4fe930905f2c
Josh Hofing <joshhofing@gmail.com>
2015-05-10 14:09:19 -0400

    Move iteration stuff into a separate file

--- pyutil.py
+++ pyutil.py
@@ -1,39 +1,7 @@
 from itertools import izip, combinations
 from string import printable
 from chain import *
-
-def groups_of(itr, n=8):
-    """ Return groups of n consecutive elements from itr
-        n defaults to 8
-
-        Returns a generator
-    """
-    return izip(*[iter(itr)]*n)
-
-def every_nth(itr, n=8, offset=0):
-    """ Return every nth element of itr, starting with offset
-        n defaults to 8, offset 0
-
-        offset must be less than n
-
-        Returns a generator
-    """
-    return (group[offset] for group in groups_of(itr, n))
-
-def window(itr, n=8):
-    """ Returns a sliding window over itr, giving access to
-        n consecutive elements at a time.
-
-        n defaults to 8
-
-        Returns a generator
-    """
-    prevs = []
-    for item in itr:
-        prevs.append(item)
-        if len(prevs) == n:
-            yield prevs
-            prevs = prevs[1:]
+from iteration import *
 
 def join(s, joiner=""):
     """ Sugar to join strings in a more sensible order
