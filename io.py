875e53f795ba9ca06987383ba0e81b0b5e9984b3
Josh Hofing <joshhofing@gmail.com>
2015-05-16 16:35:32 -0400

    Add repl() function

--- io.py
+++ io.py
@@ -11,3 +11,15 @@ def save_file(filename, contents):
     with open(filename, 'w') as f:
         f.write(contents)
 
+def repl(prompt=">>> "):
+    """ Start a repl using eval, call close_repl() to kill it
+    """
+    running = [True] # we need to make this a list because stupid scoping rules
+    def close_repl():
+        running[0] = False
+    print "Starting repl, call close_repl() to exit"
+    while running[0]:
+        try:
+            print(eval(raw_input(prompt)))
+        except e:
+            pass
