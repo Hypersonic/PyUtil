42c6385f3d4579432c024a7617c332ac68cff514
Josh Hofing <joshhofing@gmail.com>
2015-05-18 13:05:02 -0400

    Caesar cipher

--- pyutil.py
+++ pyutil.py
@@ -1,3 +1,4 @@
 from chain import *
 from iteration import *
 from io import *
+from text import *
---
+++ text.py
@@ -0,0 +1,16 @@
+from string import lowercase, uppercase
+
+def caesar(s, n=1):
+    """ simple caesar cipher
+    """
+    out = ""
+    for c in s:
+        if c in uppercase:
+            base = 'A'
+        elif c in lowercase:
+            base = 'a'
+        else: # not a letter, just push it on
+            out += c
+            continue
+        out += chr((ord(c) - ord(base) + n) % 26 + ord(base))
+    return out
