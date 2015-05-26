37f3957a86b4c14b8505db6e03fcd83acd2fde54
Josh Hofing <joshhofing@gmail.com>
2015-05-26 18:41:09 -0400

    Use a list for efficiency in caesar

--- text.py
+++ text.py
@@ -3,17 +3,17 @@ from string import lowercase, uppercase
 def caesar(s, n=1):
     """ simple caesar cipher
     """
-    out = ""
+    out = []
     for c in s:
         if c in uppercase:
             base = 'A'
         elif c in lowercase:
             base = 'a'
         else: # not a letter, just push it on
-            out += c
+            out.append(c)
             continue
-        out += chr((ord(c) - ord(base) + n) % 26 + ord(base))
-    return out
+        out.append(chr((ord(c) - ord(base) + n) % 26 + ord(base)))
+    return "".join(out)
 
 def substitute(s, mapping):
     """ Substitute any keys in mapping with their value
