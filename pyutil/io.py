b92ef0c505f919ee9b7b8917ed58b1cd45e7303f
Josh Hofing <joshhofing@gmail.com>
2015-05-28 14:05:25 -0400

    Use the real repl implementation for repl

--- pyutil/io.py
+++ pyutil/io.py
@@ -11,16 +11,6 @@ def save_file(filename, contents):
     with open(filename, 'w') as f:
         f.write(contents)
 
-def repl(prompt=">>> "):
-    """ Start a repl using eval, call close_repl() to kill it
-    """
-    running = [True] # we need to make this a list because stupid scoping rules
-    def close_repl():
-        running[0] = False
-    print "Starting repl, call close_repl() to exit"
-    while running[0]:
-        i = raw_input(prompt)
-        try:
-            print(eval(i))
-        except:
-            print "err:",i
+from code import interact
+def repl():
+    interact(local=locals())
